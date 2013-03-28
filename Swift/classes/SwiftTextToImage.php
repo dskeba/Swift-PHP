1ca9edc363b9e702149e52f26cb4abf7408d9abb
Derek Skeba <derek@mediavim.com>
2013-03-28 00:27:29 -0400

    Added primary functions to SwiftTextToImage class.

--- Swift/classes/SwiftTextToImage.php
+++ Swift/classes/SwiftTextToImage.php
@@ -36,16 +36,61 @@
 
 /**
  * The SwiftTextToImage class contains functions to help us create and write
- * text onto images dynamically.
+ * text onto images dynamically. (Note: This class requires the GB Library).
  * @package Swift
  */
 class SwiftTextToImage {
+	
+	// private class variables
+	$m_img = null;
 	
 	/**
 	 * Creates a new SwiftTextToImage object.
 	 * @return SwiftTextToImage The new SwiftTextToImage object.
 	 */
-	public function __construct() {}
+	public function __construct($width, $height) {
+		$this->m_img = ImageCreate($width, $height);
+	}
+	
+	/**
+	 * Draws text with the given properties onto the image.
+	 * @return Boolean True on success, otherwise False.
+	 */
+	public function drawText($text, $size, $red, $green, $blue) {
+		// Calculate the fonts width and height
+		$font_width = ImageFontWidth($size);
+		$font_height = ImageFontHeight($size);
+		// Find width and height of our text
+		$text_width = $font_width * strlen($text);
+		$text_height = $font_height;
+		// Position to align in center width
+		$position_x = ceil(($width - $text_width) / 2);
+		// Position to align in center height
+		$position_y = ceil(($height - $text_height) / 2);
+		// Create text color
+		$text_color = ImageColorAllocate($this->$m_img, $red, $green, $blue);
+		// Render the text onto the image object and return the result
+		return ImageString($this->m_img, $size, $position_x, $position_y, $text, $text_color);
+	}
+	
+	/**
+	 * Draws background with the given properties onto the image.
+	 * @return Boolean True on success, otherwise False.
+	 */
+	public function drawBackground($red, $green, $blue) {
+		// Set color to render background
+		$bg = ImageColorAllocate($this->m_img, $red, $green, $blue);
+		return ImageRectangle($im, 0, 0, $width, $height, $bg);
+	}
+	
+	/**
+	 * Render and output the image as a PNG file.
+	 * @return Boolean True on success, otherwise False.
+	 */
+	public function renderPng() {
+		header("Content-type: image/png");
+		return ImagePNG($this->m_img);
+	}
 	
 }
 
