408149d8c7a8526d0c71a077ca833191fc3c8503
Derek Skeba <derek@mediavim.com>
2013-04-08 15:09:41 -0400

    Added many new SwiftImage methods and made params accept more types.

--- Swift/classes/SwiftImage.php
+++ Swift/classes/SwiftImage.php
@@ -67,6 +67,14 @@ class SwiftImage {
 	}
 	
 	/**
+	 * Returns the GD image resource.
+	 * @return Resource The image resource.
+	 */
+	public function getResource() {
+		return $this->m_img;
+	}
+	
+	/**
 	 * Returns the width in pixels of the current image resource.
 	 * @return Integer The pixel width of the image.
 	 */
@@ -123,11 +131,23 @@ class SwiftImage {
 	}
 	
 	/**
+	 * Sets the given color as transparent on the image pallette.
+	 * @param Array $color An array containing color values from 0 to 255 for the keys: 'red', 'green', and 'blue'.
+	 * @return Boolean True on success, otherwise False.
+	 */
+	function setTransparentColor($color = array('red' => '0', 'green' => '0', 'blue' => '0')) {
+		// Create our color resource
+		$transparent_color = imagecolorallocate($this->m_img, $color['red'], $color['green'], $color['blue']);
+		// Set the given color to be transparent
+		return imagecolortransparent($this->m_img, $transparent_color);
+	}
+	
+	/**
 	 * Draws text with the given properties onto the image.
 	 * @param String $text The text to write onto the image.
 	 * @param Integer $size The size of the text.
 	 * @param Array $color An array containing color values from 0 to 255 for the keys: 'red', 'green', 'blue', and 'alpha'.
-	 * @param String $position Sets the position of the text on the image. (center, top, bottom, left, right, topleft, bottomleft, topright, bottomright)
+	 * @param Mixed $position Sets the position of the text on the image. Use String value to specify relative position: center, top, bottom, left, right, topleft, bottomleft, topright, bottomright. Otherwise, provide Array with X and Y keys describing coordinates.
 	 * @param String $font Sets the font type. (arial, comicsans, couriernew, georgia, tahoma, timesnewroman, verdana)
 	 * @param Integer $x_padding Sets the x coordinate padding when positioning the text. (Default: 10)
 	 * @param Integer $y_padding Sets the Y coordinate padding when positioning the text. (Default: 10)
@@ -146,56 +166,85 @@ class SwiftImage {
 		// Add the path to our select font
 		$font = FW_INCLUDES_DIR . '/fonts/' . $font . '.ttf';
 		// Position our text on the image
-		if ($position == 'center') {
-			// Align x at center
-			$position_x = ceil(($this->getWidth() - $text_width) / 2);
-			// Align y at center
-			$position_y = ceil(($this->getHeight() + $text_height) / 2);
-		} else if ($position == 'top') {
-			// Align x at center
-			$position_x = ceil(($this->getWidth() - $text_width) / 2);
-			// Align y at top
-			$position_y = $y_padding + $text_height;
-		} else if ($position == 'bottom') {
-			// Align x at center
-			$position_x = ceil(($this->getWidth() - $text_width) / 2);
-			// Align y at bottom
-			$position_y = $this->getHeight() - $y_padding;
-		} else if ($position == 'left') {
-			// Align x at left
-			$position_x = $x_padding;
-			// Align y at center
-			$position_y = ceil(($this->getHeight() + $text_height) / 2);
-		} else if ($position == 'right') {
-			// Align x at right
-			$position_x = $this->getWidth() - $text_width - $x_padding;
-			// Align y at center
-			$position_y = ceil(($this->getHeight() + $text_height) / 2);
-		} else if ($position == 'topleft') {
-			// Align x at left
-			$position_x = $x_padding;
-			// Align y at top
-			$position_y = $y_padding + $text_height;
-		} else if ($position == 'bottomleft') {
-			// Align x at left
-			$position_x = $x_padding;
-			// Align y at bottom
-			$position_y = $this->getHeight() - $y_padding;
-		} else if ($position == 'topright') {
-			// Align x at right
-			$position_x = $this->getWidth() - $text_width - $x_padding;
-			// Align y at top
-			$position_y = $y_padding + $text_height;
-		} else if ($position == 'bottomright') {
-			// Align x at right
-			$position_x = $this->getWidth() - $text_width - $x_padding;
-			// Align y at bottom
-			$position_y = $this->getHeight() - $y_padding;
+		if (gettype($position) == 'array') {
+			$position_x = $position['x'];
+			$position_y = $position['y'];
+		} else if (gettype($position) == 'string') {
+			if ($position == 'center') {
+				// Align x at center
+				$position_x = ceil(($this->getWidth() - $text_width) / 2);
+				// Align y at center
+				$position_y = ceil(($this->getHeight() + $text_height) / 2);
+			} else if ($position == 'top') {
+				// Align x at center
+				$position_x = ceil(($this->getWidth() - $text_width) / 2);
+				// Align y at top
+				$position_y = $y_padding + $text_height;
+			} else if ($position == 'bottom') {
+				// Align x at center
+				$position_x = ceil(($this->getWidth() - $text_width) / 2);
+				// Align y at bottom
+				$position_y = $this->getHeight() - $y_padding;
+			} else if ($position == 'left') {
+				// Align x at left
+				$position_x = $x_padding;
+				// Align y at center
+				$position_y = ceil(($this->getHeight() + $text_height) / 2);
+			} else if ($position == 'right') {
+				// Align x at right
+				$position_x = $this->getWidth() - $text_width - $x_padding;
+				// Align y at center
+				$position_y = ceil(($this->getHeight() + $text_height) / 2);
+			} else if ($position == 'topleft') {
+				// Align x at left
+				$position_x = $x_padding;
+				// Align y at top
+				$position_y = $y_padding + $text_height;
+			} else if ($position == 'bottomleft') {
+				// Align x at left
+				$position_x = $x_padding;
+				// Align y at bottom
+				$position_y = $this->getHeight() - $y_padding;
+			} else if ($position == 'topright') {
+				// Align x at right
+				$position_x = $this->getWidth() - $text_width - $x_padding;
+				// Align y at top
+				$position_y = $y_padding + $text_height;
+			} else if ($position == 'bottomright') {
+				// Align x at right
+				$position_x = $this->getWidth() - $text_width - $x_padding;
+				// Align y at bottom
+				$position_y = $this->getHeight() - $y_padding;
+			}
+		} else {
+			return false;
 		}
 		// Create text color
 		$text_color = imagecolorallocatealpha($this->m_img, $color['red'], $color['green'], $color['blue'], $color['alpha']);
 		// Render the text onto the image object and return the result
 		return imagettftext($this->m_img, $size, $angle, $position_x, $position_y, $text_color, $font, $text);
+	}
+	
+	/**
+	 * Draws a SwiftImage object onto the current SwiftImage object.
+	 * @param SwiftImage $swift_image A SwiftImage object to copy onto the current SwiftImage.
+	 * @param Integer $dest_x The x-coordinate value to draw at.
+	 * @param Integer $dest_y The y-coordinate value to draw at.
+	 * @param Integer $src_width The width in pixels to retrieve of the source.
+	 * @param Integer $src_height The width in pixels to retrieve of the source.
+	 * @param Integer $alpha_pct The alpha percentage of image between 0 and 100. (0 = Transparent, 100 = Opaque)
+	 * @return Boolean True on success, otherwise False.
+	 */
+	public function drawSwiftImage($swift_image, $dest_x, $dest_y, $src_width = null, $src_height = null, $alpha_pct = 100) {
+		$src_img = $swift_image->getResource();
+		$dest_img = $this->m_img;
+		if ($src_width == null) {
+			$src_width = $swift_image->getWidth();
+		}
+		if ($src_height == null) {
+			$src_height = $swift_image->getHeight();
+		}
+		return imagecopymerge($dest_img, $src_img, $dest_x, $dest_y, 0, 0, $src_width, $src_height, $alpha_pct);
 	}
 	
 	/**
@@ -205,7 +254,23 @@ class SwiftImage {
 	 */
 	public function drawBackground($color = array('red' => '255', 'green' => '255', 'blue' => '255', 'alpha' => '0')) {
 		// Draw the background and return true on success.
-		return $this->drawRectangle(0, 0, $this->getWidth(), $this->getHeight(), $color);
+		return $this->drawFilledRectangle(0, 0, $this->getWidth(), $this->getHeight(), $color);
+	}
+	
+	/**
+	 * Draws a filled rectangle with the given properties onto the image.
+	 * @param Integer $x1 The x coordinate of the upper left point of the rectangle.
+	 * @param Integer $y1 The y coordinate of the upper left point of the rectangle.
+	 * @param Integer $x2 The x coordinate of the lower right point of the rectangle.
+	 * @param Integer $y2 The y coordinate of the lower right point of the rectangle.
+	 * @param Array $color An array containing color values from 0 to 255 for the keys: 'red', 'green', 'blue', and 'alpha'.
+	 * @return Boolean True on success, otherwise False.
+	 */
+	public function drawFilledRectangle($x1, $y1, $x2, $y2, $color = array('red' => '0', 'green' => '0', 'blue' => '0', 'alpha' => '0')) {
+		// Create the background color
+		$bg = imagecolorallocatealpha($this->m_img, $color['red'], $color['green'], $color['blue'], $color['alpha']);
+		// Draw the rectangle and return true on success.
+		return imagefilledrectangle($this->m_img, $x1, $y1, $x2, $y2, $bg);
 	}
 	
 	/**
@@ -215,13 +280,51 @@ class SwiftImage {
 	 * @param Integer $x2 The x coordinate of the lower right point of the rectangle.
 	 * @param Integer $y2 The y coordinate of the lower right point of the rectangle.
 	 * @param Array $color An array containing color values from 0 to 255 for the keys: 'red', 'green', 'blue', and 'alpha'.
+	 * @param Integer $thickness Sets the line thickness when drawing onto image. (Default: 1)
 	 * @return Boolean True on success, otherwise False.
 	 */
-	public function drawRectangle($x1, $y1, $x2, $y2, $color = array('red' => '0', 'green' => '0', 'blue' => '0', 'alpha' => '0')) {
+	public function drawRectangle($x1, $y1, $x2, $y2, $color = array('red' => '0', 'green' => '0', 'blue' => '0', 'alpha' => '0'), $thickness = 1) {
 		// Create the background color
 		$bg = imagecolorallocatealpha($this->m_img, $color['red'], $color['green'], $color['blue'], $color['alpha']);
+		// Set the line thickness
+		imagesetthickness($this->m_img, $thickness);
 		// Draw the rectangle and return true on success.
-		return imagefilledrectangle($this->m_img, $x1, $y1, $x2, $y2, $bg);
+		return imagerectangle($this->m_img, $x1, $y1, $x2, $y2, $bg);
+	}
+	
+	/**
+	 * Draws a filled ellipse with the given properties onto the image.
+	 * @param Integer $x The x coordinate of the center of the ellipse.
+	 * @param Integer $y The y coordinate of the center of the ellipse.
+	 * @param Integer $width The width in pixels of the ellipse.
+	 * @param Integer $height The height in pixels of the ellipse.
+	 * @param Array $color An array containing color values from 0 to 255 for the keys: 'red', 'green', 'blue', and 'alpha'.
+	 * @return Boolean True on success, otherwise False.
+	 */
+	public function drawFilledEllipse($x, $y, $width, $height, $color = array('red' => '0', 'green' => '0', 'blue' => '0', 'alpha' => '0')) {
+		// Create the background color
+		$ellipse_color = imagecolorallocatealpha($this->m_img, $color['red'], $color['green'], $color['blue'], $color['alpha']);
+		// Draw the rectangle and return true on success.
+		return imagefilledellipse($this->m_img, $x, $y, $width, $height, $ellipse_color);
+	}
+	
+	/**
+	 * Draws a ellipse with the given properties onto the image.
+	 * @param Integer $x The x coordinate of the center of the ellipse.
+	 * @param Integer $y The y coordinate of the center of the ellipse.
+	 * @param Integer $width The width in pixels of the ellipse.
+	 * @param Integer $height The height in pixels of the ellipse.
+	 * @param Array $color An array containing color values from 0 to 255 for the keys: 'red', 'green', 'blue', and 'alpha'.
+	 * @param Integer $thickness Sets the line thickness when drawing onto image. (Default: 1)
+	 * @return Boolean True on success, otherwise False.
+	 */
+	public function drawEllipse($x, $y, $width, $height, $color = array('red' => '0', 'green' => '0', 'blue' => '0', 'alpha' => '0'), $thickness = 1) {
+		// Create the background color
+		$ellipse_color = imagecolorallocatealpha($this->m_img, $color['red'], $color['green'], $color['blue'], $color['alpha']);
+		// Set the line thickness
+		imagesetthickness($this->m_img, $thickness);
+		// Draw the rectangle and return true on success.
+		return imageellipse($this->m_img, $x, $y, $width, $height, $ellipse_color);
 	}
 	
 	/**
@@ -231,11 +334,14 @@ class SwiftImage {
 	 * @param Integer $x2 The x coordinate of the ending point of the line.
 	 * @param Integer $y2 The y coordinate of the ending point of the line.
 	 * @param Array $color An array containing color values from 0 to 255 for the keys: 'red', 'green', 'blue', and 'alpha'.
+	 * @param Integer $thickness Sets the line thickness when drawing onto image. (Default: 1)
 	 * @return Boolean True on success, otherwise False.
 	 */
-	public function drawLine($x1, $y1, $x2, $y2, $color = array('red' => '0', 'green' => '0', 'blue' => '0', 'alpha' => '0')) {
+	public function drawLine($x1, $y1, $x2, $y2, $color = array('red' => '0', 'green' => '0', 'blue' => '0', 'alpha' => '0'), $thickness = 1) {
 		// Create the line color
 		$line_color = imagecolorallocatealpha($this->m_img, $color['red'], $color['green'], $color['blue'], $color['alpha']);
+		// Set the line thickness
+		imagesetthickness($this->m_img, $thickness);
 		// Draw the line and return true on succcess.
 		return imageline($this->m_img, $x1, $y1, $x2, $y2, $line_color);
 	}
@@ -245,18 +351,19 @@ class SwiftImage {
 	 * @param Integer $x_spacing Number of pixels to space between vertical lines of grid.
 	 * @param Integer $y_spacing Number of pixels to space between horizontal lines of grid.
 	 * @param Array $color An array containing color values from 0 to 255 for the keys: 'red', 'green', 'blue', and 'alpha'.
+	 * @param Integer $thickness Sets the line thickness when drawing onto image. (Default: 1)
 	 * @param Integer $offset Value to offset the angle of the grid lines.
 	 * @return Boolean True on success, otherwise False.
 	 */
-	public function drawGrid($x_spacing, $y_spacing, $color = array('red' => '0', 'green' => '0', 'blue' => '0', 'alpha' => '0'), $offset = 0) {
+	public function drawGrid($x_spacing, $y_spacing, $color = array('red' => '0', 'green' => '0', 'blue' => '0', 'alpha' => '0'), $thickness = 1, $offset = 0) {
 		$offset = ($offset / 100) * 20;
 		$x_count = $this->getWidth() / $x_spacing;
 		$y_count = $this->getHeight() / $y_spacing;
 		for ($i = -1; $i < $x_count + 1; $i++) {
-			$this->drawLine(($i * $x_spacing) - $offset, 0, ($i * $x_spacing) + $offset, $this->getHeight(), $color);
+			$this->drawLine(($i * $x_spacing) - $offset, 0, ($i * $x_spacing) + $offset, $this->getHeight(), $color, $thickness);
 		}
 		for ($i = -1; $i < $y_count + 1; $i++) {
-			$this->drawLine(0, ($i * $y_spacing) - $offset, $this->getWidth(), ($i * $y_spacing) + $offset, $color);
+			$this->drawLine(0, ($i * $y_spacing) - $offset, $this->getWidth(), ($i * $y_spacing) + $offset, $color, $thickness);
 		}
 	}
 	
