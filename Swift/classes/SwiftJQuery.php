30a79f066929b74e9054aae2a7ca9704e05d0ee8
Derek Skeba <derek@mediavim.com>
2013-05-01 14:49:24 -0400

    Cleaned up script output and added more methods to SwiftJQuery.

--- Swift/classes/SwiftJQuery.php
+++ Swift/classes/SwiftJQuery.php
@@ -69,90 +69,142 @@ class SwiftJQuery {
 				</script>\n";
 	}
 
-	public function createAjaxFunction($func_name, $url, $data_ids, $method = "get", $load_id) {
-		$script .= '<script type="text/javascript">\n';
-		$script .= '	function ' . $func_name . '() {\n';
-		$script .= '		var data = {\n';
-		$count = count($data_ids);
-		foreach ($data_ids as $key => $value) {
-			$script .= '				"' . $key . '":"' . $value . '"';
+	public function createAjaxFunction($func_name, $url, $method = "get", $ids_with_data, $id_to_set) {
+		$script .= "<script type=\"text/javascript\">\n";
+		$script .= "	function " . $func_name . "() {\n";
+		$script .= "		var data = {\n";
+		$count = count($ids_with_data);
+		foreach ($ids_with_data as $key => $value) {
+			$script .= "				\"" . $key . "\":\"" . $value . "\"";
 			$cur = $cur + 1;
 			if ($cur < ($count - 1)) {
-				$script .= ',\n';
+				$script .= ",\n";
 			} else {
-				$script .= '		};\n';
+				$script .= "		};\n";
 			}
 		}
-		$script .= '		$.' . $method . '("' . $url . '", data, function(data, status, xhr) {\n';
-		$script .= '			if (status == "success") {\n';
-		$script .= '				$("#' . $load_id . '").html(data);\n';
-		$script .= '			} else {\n';
-		$script .= '				console.log("Error: " + xhr.status + " " + xhr.statusText);\n';
-		$script .= '			}\n';
-		$script .= '		});\n';
-		$script .= '	}\n';
-		$script .= '</script>\n';
-		return $script;
+		$script .= "		$." . $method . "(\"" . $url . "\", data, function(data, status, xhr) {\n";
+		$script .= "			if (status == \"success\") {\n";
+		$script .= "				$(\"#" . $id_to_set . "\").html(data);\n";
+		$script .= "			} else {\n";
+		$script .= "				console.log(\"Error: \" + xhr.status + \" \" + xhr.statusText);\n";
+		$script .= "			}\n";
+		$script .= "		});\n";
+		$script .= "	}\n";
+		$script .= "</script>\n";
+		return "\n" . $script . "\n";
 	}
 	
-	public function createAjaxPostFunction($func_name, $url, $data_ids, $load_id) {
-		return $this->createAjaxFunction($func_name, $url, $data_ids, 'post', $load_id);
+	public function createAjaxPostFunction($func_name, $url, $ids_with_data, $id_to_set) {
+		return $this->createAjaxFunction($func_name, $url, $ids_with_data, 'post', $id_to_set);
 	}
 	
-	public function createAjaxGetFunction($func_name, $url, $data_ids, $load_id) {
-		return $this->createAjaxFunction($func_name, $url, $data_ids, 'get', $load_id);
+	public function createAjaxGetFunction($func_name, $url, $ids_with_data, $id_to_set) {
+		return $this->createAjaxFunction($func_name, $url, $ids_with_data, 'get', $id_to_set);
 	}
 	
-	public function createEventHook($id, $event, $func_name) {
-		return '<script type="text/javascript">
-					$(document).ready(function(){
-						$("#' . $id . '").' . $event . '(' . $func_name . ');
-					});
-				</script>\n';
+	public function createEventHook($id, $event, $callback) {
+		$script .= "<script type=\"text/javascript\">\n";
+		$script .= "		$(document).ready(function(){\n";
+		$script .= "			$(\"#" . $id . "\")." . $event . "(" . $callback . ");\n";
+		$script .= "		});\n";
+		$script .= "</script>\n";
+		return "\n" . $script . "\n";
 	}
 	
-	public function createIntervalHook($delay, $func_name, $interval_var) {
-		return '<script type="text/javascript">
-					$(document).ready(function(){
-						var ' . $interval_var . ' = setInterval(function(){
-							' . $func_name . '();
-						}, ' . $delay . ');
-					});
-				</script>';
+	public function createIntervalHook($delay, $callback, $interval_var = 'interval_var') {
+		$script .= "<script type=\"text/javascript\">\n";
+		$script .= "	var " . $interval_var . ";\n";
+		$script .= "		$(document).ready(function(){\n";
+		$script .= "			" . $interval_var . " = setInterval(function(){\n";
+		$script .= "				" . $callback . "();\n";
+		$script .= "			}, " . $delay . ");\n";
+		$script .= "		});\n";
+		$script .= "</script>\n";
+		return "\n" . $script . "\n";
 	}
 	
-	public function createIntervalClearFunction($func_name, $interval_var) {
-		return '<script type="text/javascript">
-					function ' . $func_name . '() {
-						window.clearInterval(' . $interval_var . ');
-					}
-				</script>';
+	public function createIntervalClearFunction($func_name, $interval_var = 'interval_var') {
+		$script .= "<script type=\"text/javascript\">\n";
+		$script .= "	function " . $func_name . "() {\n";
+		$script .= "		window.clearInterval(" . $interval_var . ");\n";
+		$script .= "	}\n";
+		$script .= "</script>\n";
+		return "\n" . $script . "\n";
 	}
 	
-	public function createTimeoutHook($delay, $func_name, $interval_var) {
-		return '<script type="text/javascript">
-					$(document).ready(function(){
-						var ' . $interval_var . ' = setTimeout(function(){
-							' . $func_name . '();
-						}, ' . $delay . ');
-					});
-				</script>';
+	public function createTimeoutHook($delay, $callback, $timeout_var = 'timeout_var') {
+		$script .= "<script type=\"text/javascript\">\n";
+		$script .= "	var " . $timeout_var . ";\n";
+		$script .= "		$(document).ready(function(){\n";
+		$script .= "			" . $timeout_var . " = setTimeout(function(){\n";
+		$script .= "				" . $callback . "();\n";
+		$script .= "			}, " . $delay . ");\n";
+		$script .= "		});\n";
+		$script .= "</script>\n";
+		return "\n" . $script . "\n";
 	}
 	
-	public function createTimeoutClearFunction($func_name, $interval_var) {
-		return '<script type="text/javascript">
-					function ' . $func_name . '() {
-						window.clearTimeout(' . $interval_var . ');
-					}
-				</script>';
+	public function createTimeoutClearFunction($func_name, $timeout_var = 'timeout_var') {
+		$script .= "<script type=\"text/javascript\">\n";
+		$script .= "	function " . $func_name . "() {\n";
+		$script .= "		window.clearTimeout(" . $timeout_var . ");\n";
+		$script .= "	}\n";
+		$script .= "</script>\n";
+		return "\n" . $script . "\n";
+	}
+	
+	public function createSetFunction($func_name, $id_to_set, $id_to_get) {
+		$script .= "<script type=\"text/javascript\">\n";
+		$script .= "	function " . $func_name . "() {\n";
+		$script .= "		$(\"#" . $id_to_set . "\").html($(\"#" . $id_to_get . "\").html());\n";
+		$script .= "	}\n";
+		$script .= "</script>\n";
+		return "\n" . $script . "\n";
+	}
+	
+	public function createClearFunction($func_name, $id_to_clear) {
+		$script .= "<script type=\"text/javascript\">\n";
+		$script .= "	function " . $func_name . "() {\n";
+		$script .= "		$(\"#" . $id_to_clear . "\").html(\"\");\n";
+		$script .= "	}\n";
+		$script .= "</script>\n";
+		return "\n" . $script . "\n";
+	}
+	
+	public function createHideFunction($func_name, $id_to_hide, $animation_time = 1000, $callback = null) {
+		$script .= "<script type=\"text/javascript\">\n";
+		$script .= "	function " . $func_name . "() {\n";
+		if ($callback) {
+			$script .= "		$(\"#" . $id_to_hide . "\").hide(" . $animation_time . ", " . $callback . ");\n";
+		} else {
+			$script .= "		$(\"#" . $id_to_hide . "\").hide(" . $animation_time . ");\n";
+		}
+		$script .= "	}\n";
+		$script .= "</script>\n";
+		return "\n" . $script . "\n";
+	}
+	
+	public function createShowFunction($func_name, $id_to_show, $animation_time = 1000, $callback = null) {
+		$script .= "<script type=\"text/javascript\">\n";
+		$script .= "	function " . $func_name . "() {\n";
+		if ($callback) {
+			$script .= "		$(\"#" . $id_to_show . "\").show(" . $animation_time . ", " . $callback . ");\n";
+		} else {
+			$script .= "		$(\"#" . $id_to_show . "\").show(" . $animation_time . ");\n";
+		}
+		$script .= "	}\n";
+		$script .= "</script>\n";
+		return "\n" . $script . "\n";
 	}
 	
 	public function createAlertFunction($func_name, $msg) {
-		return '<script type="text/javascript">
-					function ' . $func_name . '() {
-						alert("' . $msg . '");
-					}
-				</script>';
+		$script .= "<script type=\"text/javascript\">\n";
+		$script .= "	function " . $func_name . "() {\n";
+		$script .= "		alert(\"" . $msg . "\");\n";
+		$script .= "	}\n";
+		$script .= "</script>\n";
+		return "\n" . $script . "\n";
 	}
 	
 }
