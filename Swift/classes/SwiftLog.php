bbb50e792a064c0ada1b44ff16def612aa3f70be
Derek Skeba <derek@mediavim.com>
2013-09-02 18:33:58 -0400

    Created the SwiftLog Class.

---
+++ Swift/classes/SwiftLog.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * SwiftLog.php
+ *
+ * This file contains the SwiftLog class.
+ *
+ * @author Derek Skeba
+ * @copyright 2012 - 2013 Media Vim LLC
+ * @link http://swiftphp.org
+ * @license http://swiftphp.org/license
+ * @version 1.1
+ * @package Swift
+ *
+ * MIT LICENSE
+ *
+ * Copyright (c) 2012 - 2013 Media Vim LLC (http://mediavim.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining 
+ * a copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+ * SOFTWARE.
+ */
+
+/**
+ * Contains functions for reading and writing log messages to the file system
+ * or to a database.
+ * for our App.
+ * @package Swift
+ */
+class SwiftLog {
+	
+	/**
+	 * Creates a new SwiftLog object.
+	 * @return SwiftLog The new SwiftLog object
+	 */
+	public function __construct() {}
+	
+}
+
+?>
