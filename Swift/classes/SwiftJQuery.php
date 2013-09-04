568d0068e6c5715d4fb738db5c8ee3141d9a412f
Derek Skeba <derek@mediavim.com>
2013-09-03 20:15:18 -0400

    Added comments and PHP Docs to the SwiftJQuery Class.

--- Swift/classes/SwiftJQuery.php
+++ Swift/classes/SwiftJQuery.php
@@ -52,7 +52,7 @@ class SwiftJQuery {
 	 * with the specified CSS classes for the parent buttons and child buttons.
 	 * @param string $top_class The CSS class for the top/parent buttons of your menu
 	 * @param string $sub_class The CSS class for the sub/child buttons of your menu
-	 * @return string A HTML compatible script tag with the JS code.
+	 * @return string A HTML compatible script tag with the JavaScript code.
 	 */
 	public function createDropDownMenu($top_class, $sub_class) {
 		$script .= "<script type=\"text/javascript\">\n";
@@ -70,6 +70,14 @@ class SwiftJQuery {
 		return "\n" . $script . "\n";
 	}
 
+	/**
+	 * Creates an Ajax Callback function which can be passed into the createAjaxFunction method.
+	 * @param string $func_name The name of the function.
+	 * @param string $id The ID of the element to perform action on.
+	 * @param string $action The JQuery action to perform on the $id provided. Default: html
+	 * @param string $callback The name of a JavaScript function to call after completing AJAX callback function. (Optional)
+	 * @return string A HTML compatible script tag with the JavaScript code.
+	 */
 	public function createAjaxCallback($func_name, $id, $action = 'html', $callback = null) {
 		$script .= "<script type=\"text/javascript\">\n";
 		$script .= "	function " . $func_name . "(data, status, xhr) {\n";
@@ -93,6 +101,12 @@ class SwiftJQuery {
 		return "\n" . $script . "\n";
 	}
 	
+	/**
+	 * Creates a JavaScript function that upon execution with call all of the function provided in the $func_array.
+	 * @param string $func_name The name of the function.
+	 * @param array $func_array An array of JavaScript function names, $func_array[0] = $value, where $value is the function name.
+	 * @return string A HTML compatible script tag with the JavaScript code.
+	 */
 	public function createChainFunction($func_name, $func_array) {
 		$script .= "<script type=\"text/javascript\">\n";
 		$script .= "	function " . $func_name . "() {\n";
@@ -104,6 +118,15 @@ class SwiftJQuery {
 		return "\n" . $script . "\n";
 	}
 	
+	/**
+	 * Creates and returns an AJAX function.
+	 * @param string $func_name The name of the function.
+	 * @param string $url The URL for the AJAX call.
+	 * @param string $method The request method for the AJAX call. Default: get
+	 * @param array $data_ids An array of HTML element ID's whose value's will be used as query variables for the AJAX call.
+	 * @param string $callback The name of a AJAX callback function (Optional)
+	 * @return string A HTML compatible script tag with the JavaScript code.
+	 */
 	public function createAjaxFunction($func_name, $url, $method = "get", $data_ids, $callback) {
 		$script .= "<script type=\"text/javascript\">\n";
 		$script .= "	function " . $func_name . "() {\n";
@@ -125,14 +148,37 @@ class SwiftJQuery {
 		return "\n" . $script . "\n";
 	}
 	
+	/**
+	 * Creates and returns an AJAX function using the POST method.
+	 * @param string $func_name The name of the function.
+	 * @param string $url The URL for the AJAX call.
+	 * @param array $data_ids An array of HTML element ID's whose value's will be used as query variables for the AJAX call.
+	 * @param string $callback The name of a AJAX callback function (Optional)
+	 * @return string A HTML compatible script tag with the JavaScript code.
+	 */
 	public function createAjaxPostFunction($func_name, $url, $data_ids, $callback) {
 		return $this->createAjaxFunction($func_name, $url, 'post', $data_ids, $callback);
 	}
 	
+	/**
+	 * Creates and returns an AJAX function using the GET method.
+	 * @param string $func_name The name of the function.
+	 * @param string $url The URL for the AJAX call.
+	 * @param array $data_ids An array of HTML element ID's whose value's will be used as query variables for the AJAX call.
+	 * @param string $callback The name of a AJAX callback function (Optional)
+	 * @return string A HTML compatible script tag with the JavaScript code.
+	 */
 	public function createAjaxGetFunction($func_name, $url, $data_ids, $callback) {
 		return $this->createAjaxFunction($func_name, $url, 'get', $data_ids, $callback);
 	}
 	
+	/**
+	 * Creates a JQuery event on the specified $id to execute the $callback function.
+	 * @param string $id The ID of the HTML element to put the event hook on.
+	 * @param string $event The type of JQuery event/action to hook on the element.
+	 * @param string $callback The name of the JavaScript function to call when event occurs on the element.
+	 * @return string A HTML compatible script tag with the JavaScript code.
+	 */
 	public function createEventHook($id, $event, $callback) {
 		$script .= "<script type=\"text/javascript\">\n";
 		$script .= "		$(document).ready(function(){\n";
@@ -142,6 +188,13 @@ class SwiftJQuery {
 		return "\n" . $script . "\n";
 	}
 	
+	/**
+	 * Creates a JavaScript timer that executes the $callback function every $delay milliseconds.
+	 * @param integer $delay The number of milliseconds to delay between calls
+	 * @param string $callback The name of the JavaScript function to call.
+	 * @param string $interval_var The name of the JavaScript variable to store the timer in. Default = interval_var
+	 * @return string A HTML compatible script tag with the JavaScript code.
+	 */
 	public function createIntervalHook($delay, $callback, $interval_var = 'interval_var') {
 		$script .= "<script type=\"text/javascript\">\n";
 		$script .= "	var " . $interval_var . ";\n";
@@ -154,6 +207,12 @@ class SwiftJQuery {
 		return "\n" . $script . "\n";
 	}
 	
+	/**
+	 * Creates a JavaScript function that clears the specified JavaScript interval variable.
+	 * @param string $func_name The name of the JavaScript function.
+	 * @param string $interval_var The name of the JavaScript variable that holds the timer. Default = interval_var
+	 * @return string A HTML compatible script tag with the JavaScript code.
+	 */
 	public function createIntervalClearFunction($func_name, $interval_var = 'interval_var') {
 		$script .= "<script type=\"text/javascript\">\n";
 		$script .= "	function " . $func_name . "() {\n";
@@ -163,6 +222,13 @@ class SwiftJQuery {
 		return "\n" . $script . "\n";
 	}
 	
+	/**
+	 * Creates a JavaScript timer that executes the $callback once after waiting $delay milliseconds.
+	 * @param integer $delay The number of milliseconds to delay between calls
+	 * @param string $callback The name of the JavaScript function to call.
+	 * @param string $interval_var The name of the JavaScript variable to store the timer in. Default = timeout_var
+	 * @return string A HTML compatible script tag with the JavaScript code.
+	 */
 	public function createTimeoutHook($delay, $callback, $timeout_var = 'timeout_var') {
 		$script .= "<script type=\"text/javascript\">\n";
 		$script .= "	var " . $timeout_var . ";\n";
@@ -175,6 +241,12 @@ class SwiftJQuery {
 		return "\n" . $script . "\n";
 	}
 	
+	/**
+	 * Creates a JavaScript function that clears the specified JavaScript timeout variable.
+	 * @param string $func_name The name of the JavaScript function.
+	 * @param string $interval_var The name of the JavaScript variable that holds the timer. Default = timeout_var
+	 * @return string A HTML compatible script tag with the JavaScript code.
+	 */
 	public function createTimeoutClearFunction($func_name, $timeout_var = 'timeout_var') {
 		$script .= "<script type=\"text/javascript\">\n";
 		$script .= "	function " . $func_name . "() {\n";
@@ -184,6 +256,13 @@ class SwiftJQuery {
 		return "\n" . $script . "\n";
 	}
 	
+	/**
+	 * Creates a JavaScript function that sets the value of $id_to_set to the value inside $id_to_get
+	 * @param string $func_name The name of the JavaScript function.
+	 * @param string $id_to_set The ID of the HTML element to set.
+	 * @param string $id_to_get The ID of the HTML element to get.
+	 * @return string A HTML compatible script tag with the JavaScript code.
+	 */
 	public function createSetFunction($func_name, $id_to_set, $id_to_get) {
 		$script .= "<script type=\"text/javascript\">\n";
 		$script .= "	function " . $func_name . "() {\n";
@@ -193,6 +272,12 @@ class SwiftJQuery {
 		return "\n" . $script . "\n";
 	}
 	
+	/**
+	 * Creates a JavaScript function that clears the current value of the $id_to_clear
+	 * @param string $func_name The name of the JavaScript function.
+	 * @param string $id_to_clear The ID of the HTML element to clear.
+	 * @return string A HTML compatible script tag with the JavaScript code.
+	 */
 	public function createClearFunction($func_name, $id_to_clear) {
 		$script .= "<script type=\"text/javascript\">\n";
 		$script .= "	function " . $func_name . "() {\n";
@@ -202,6 +287,14 @@ class SwiftJQuery {
 		return "\n" . $script . "\n";
 	}
 	
+	/**
+	 * Creates a JavaScript function hides the $id_to_hide
+	 * @param string $func_name The name of the JavaScript function.
+	 * @param string $id_to_hide The ID of the HTML element to hide.
+	 * @param integer $animation_time The number of milliseconds the hide animation should last. Default = 1000
+	 * @param string $callback The name of a JavaScript function to call after hiding the element. (Optional)
+	 * @return string A HTML compatible script tag with the JavaScript code.
+	 */
 	public function createHideFunction($func_name, $id_to_hide, $animation_time = 1000, $callback = null) {
 		$script .= "<script type=\"text/javascript\">\n";
 		$script .= "	function " . $func_name . "() {\n";
@@ -215,6 +308,14 @@ class SwiftJQuery {
 		return "\n" . $script . "\n";
 	}
 	
+	/**
+	 * Creates a JavaScript function shows the $id_to_show
+	 * @param string $func_name The name of the JavaScript function.
+	 * @param string $id_to_show The ID of the HTML element to show.
+	 * @param integer $animation_time The number of milliseconds the show animation should last. Default = 1000
+	 * @param string $callback The name of a JavaScript function to call after showing the element. (Optional)
+	 * @return string A HTML compatible script tag with the JavaScript code.
+	 */
 	public function createShowFunction($func_name, $id_to_show, $animation_time = 1000, $callback = null) {
 		$script .= "<script type=\"text/javascript\">\n";
 		$script .= "	function " . $func_name . "() {\n";
@@ -228,6 +329,12 @@ class SwiftJQuery {
 		return "\n" . $script . "\n";
 	}
 	
+	/**
+	 * Creates a JavaScript function popup an alert message.
+	 * @param string $func_name The name of the JavaScript function.
+	 * @param string $msg The message to display inside the alert box.
+	 * @return string A HTML compatible script tag with the JavaScript code.
+	 */
 	public function createAlertFunction($func_name, $msg) {
 		$script .= "<script type=\"text/javascript\">\n";
 		$script .= "	function " . $func_name . "() {\n";
