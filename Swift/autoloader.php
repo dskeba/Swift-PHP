ec9e2760dc9b0c186bfe7c7f25921bd28ce69892
Derek Skeba <derek@mediavim.com>
2013-09-03 23:49:02 -0400

    Bumped version to 1.2

--- Swift/autoloader.php
+++ Swift/autoloader.php
@@ -9,7 +9,7 @@
  * @copyright 2010 - 2013 Media Vim LLC
  * @link http://swiftphp.org
  * @license http://swiftphp.org/license
- * @version 1.1
+ * @version 1.2
  * @package Swift
  *
  * MIT LICENSE
