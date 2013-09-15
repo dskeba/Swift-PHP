48279bfbb2ea1d67cb7fcf91c565edac38026641
Derek Skeba <derek@mediavim.com>
2013-09-15 14:12:17 -0400

    Fixed removal of www in .htaccess file. Bumped version to 1.2.2

--- Swift/classes/SwiftLog.php
+++ Swift/classes/SwiftLog.php
@@ -8,7 +8,7 @@
  * @copyright 2010 - 2013 Media Vim LLC
  * @link http://swiftphp.org
  * @license http://swiftphp.org/license
- * @version 1.2.1
+ * @version 1.2.2
  * @package Swift
  *
  * MIT LICENSE
