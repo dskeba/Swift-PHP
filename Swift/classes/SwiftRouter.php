ac7c461201b4e860255df19ec669f7acb264fe19
Derek Skeba <derek@mediavim.com>
2013-10-26 23:11:30 -0400

    Bumped version to 1.3.5

--- Swift/classes/SwiftRouter.php
+++ Swift/classes/SwiftRouter.php
@@ -8,7 +8,7 @@
  * @copyright 2010 - 2013 Media Vim LLC
  * @link http://swiftphp.org
  * @license http://swiftphp.org/license
- * @version 1.3.4
+ * @version 1.3.5
  * @package Swift
  *
  * MIT LICENSE
